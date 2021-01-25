7b5fb71cee02c435aeaaf24116459d29445f4474
Abdul Rahman <rahmancam@live.com>
2021-01-25 12:21:36 +0530

    Add blocks and hashing

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use blockchainlib::*;
+
+fn main() {
+    let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis block".to_owned());
+    println!("{:?}", &block);
+
+    let h = block.hash();
+
+    println!("{:?}", &h);
+
+    block.hash = h;
+
+    println!("{:?}", &block);
+}
